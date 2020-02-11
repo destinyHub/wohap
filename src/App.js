7ba964c2b219a49243bac47f3c781727d87a4347
Stevin <michael.destiny.slayer@gmail.com>
2020-02-11 18:01:41 +0700

    Initial 2

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Container, Row, Col } from 'reactstrap';
+import './App.css';
+
+function App() {
+  return (
+    <Container>
+      <Row>
+        <Col className="mt-5">
+          <h1>Wohap</h1>
+          <p>Hai aku Wohab.. :)</p>
+        </Col>
+      </Row>
+    </Container>
+);
+}
+
+export default App;
