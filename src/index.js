bb371af6bb71a9473267b0c51b21c6886c9e7b39
Stevin <michael.destiny.slayer@gmail.com>
2020-02-11 16:55:20 +0700

    Initial one

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+ReactDOM.render(
+    <h1>Test</h1>,document.getElementById("root")
+);
+
+ReactDOM.render(
+    <h1>Test2</h1>,document.getElementById("root2")
+);
