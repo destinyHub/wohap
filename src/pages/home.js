bf5891e7813eeead324495240f70b2f44f52dd0b
Stevin <michael.destiny.slayer@gmail.com>
2020-03-03 14:26:15 +0700

    Add Carousel Folder

--- src/pages/home.js
+++ src/pages/home.js
@@ -2,12 +2,14 @@ import React, {Component} from 'react';
 import Barang from "../Components/barang";
 import Header from "../Components/header/Header";
 import { Row, Container } from 'reactstrap';
+import Carousel from "../Components/carousel/Carousel";
 
 class Home extends Component {
     render() {
         return (
             <div>
                 <Header/>
+                {/* <Carousel/> */}
                 <Barang/>
             </div>
         );
