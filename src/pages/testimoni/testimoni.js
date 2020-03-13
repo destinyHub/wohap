209854c9aa3ef9fea947416cd8da6075db6bd41c
Stevin <michael.destiny.slayer@gmail.com>
2020-03-13 21:31:53 +0700

    LOL

--- src/pages/testimoni/testimoni.js
+++ src/pages/testimoni/testimoni.js
@@ -14,14 +14,14 @@ class Testimoni extends Component {
                 <Row>
                     <Col xs="2"></Col>
                     <Col xs="8">
-                        <TestimoniCard src={FotoMike} pernyataan="Menurut saya Wohap merupakan toko furnitur yang paling lengkap di Garut,
-                                barang-barang yang dijual berkualitas dan harga cukup terjangkau." nama="Michael Stevin Cahyadi"/><br></br>
+                        <TestimoniCard src={FotoMike} pernyataan="Menurut saya Corona cukup berbahaya bagi orang. Jika kita ingin tenang dari
+                        ancaman Corona lebih baik kita menerima virusnya dan memunculkan antibodi." nama="Michael Stevin"/><br></br>
                         <TestimoniCard src={FotoIrwan} pernyataan="Menurut saya, kita tidak perlu takut terhadap Corona karena kita dilahirkan
                         sudah pasti akan menemui ajal. Jadi kita perlu mendalami iman dan menyiapkan mental untuk terkena Corona. 
                         Daripada mencemaskan lebih baik menyiapkan diri untuk terpapar Corona dan tetap hidup." nama="Johanes Irwan"/><br></br>
-                        <TestimoniCard src={FotoAlvin} pernyataan="Menurut saya, kita tidak perlu takut terhadap Corona karena kita dilahirkan
-                        sudah pasti akan menemui ajal. Jadi kita perlu mendalami iman dan menyiapkan mental untuk terkena Corona. 
-                        Daripada mencemaskan lebih baik menyiapkan diri untuk terpapar Corona dan tetap hidup." nama="Alvinus Sutendy"/><br></br>
+                        <TestimoniCard src={FotoAlvin} pernyataan="Menurut saya Corona dapat dihambat oleh suhu panas. Oleh karena itu
+                        saya sarankan teman-teman berada di lingkungan beradiasi tinggi.
+                        Semakin tinggi radiasi maka akan semakin panas badan anda. Walaupun umur menjadi pendek tapi aman dari Corona." nama="Alvinus Sutendy"/><br></br>
                     </Col>
                     <Col xs="2"></Col>
                 </Row>
