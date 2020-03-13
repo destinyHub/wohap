014c8c73d72c4d69907a5279f0b17228bdb9fb7a
Stevin <michael.destiny.slayer@gmail.com>
2020-03-13 21:20:00 +0700

    Alvin Lmao

--- src/pages/testimoni/testimoni.js
+++ src/pages/testimoni/testimoni.js
@@ -19,8 +19,9 @@ class Testimoni extends Component {
                         <TestimoniCard src={FotoIrwan} pernyataan="Menurut saya, kita tidak perlu takut terhadap Corona karena kita dilahirkan
                         sudah pasti akan menemui ajal. Jadi kita perlu mendalami iman dan menyiapkan mental untuk terkena Corona. 
                         Daripada mencemaskan lebih baik menyiapkan diri untuk terpapar Corona dan tetap hidup." nama="Johanes Irwan"/><br></br>
-                        <TestimoniCard src={FotoAlvin} pernyataan="Menurut saya Wohap merupakan toko furnitur yang paling lengkap di Garut,
-                                barang-barang yang dijual berkualitas dan harga cukup terjangkau." nama="Alvinus Sutendy"/><br></br>
+                        <TestimoniCard src={FotoAlvin} pernyataan="Menurut saya, kita tidak perlu takut terhadap Corona karena kita dilahirkan
+                        sudah pasti akan menemui ajal. Jadi kita perlu mendalami iman dan menyiapkan mental untuk terkena Corona. 
+                        Daripada mencemaskan lebih baik menyiapkan diri untuk terpapar Corona dan tetap hidup." nama="Alvinus Sutendy"/><br></br>
                     </Col>
                     <Col xs="2"></Col>
                 </Row>
